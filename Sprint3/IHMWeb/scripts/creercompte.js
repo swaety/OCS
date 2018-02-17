7159b64d4d287e609ce02b15ef5948bbd5fc2964
ibrahim_abdelkader <ibrahim.abdelkader@esprit.tn>
2018-02-17 14:16:24 +0100

    reparation creation compte js

--- Sprint3/IHMWeb/scripts/creercompte.js
+++ Sprint3/IHMWeb/scripts/creercompte.js
@@ -1,9 +1,12 @@
 function particulier(){
 	document.getElementById('choice').style.visibility='hidden';
-	document.getElementById('particulier').style.visibility='visible';
+	document.getElementById('contenu').innerHTML = "<div w3-include-html='creercompteParticulier.html'></div> ";
+	w3.includeHTML();
+
 }
 function poste(){
 	document.getElementById('choice').style.visibility='hidden';
-	document.getElementById('poste').style.visibility='visible';
+	document.getElementById('contenu').innerHTML = "<div w3-include-html='creercomptePoste.html'></div> ";
+	w3.includeHTML();
 }
 	
