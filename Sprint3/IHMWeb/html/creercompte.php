2c67f210c3247e59bf54943d828080bbd152a81f
ibrahim_abdelkader <ibrahim.abdelkader@esprit.tn>
2018-02-20 13:10:28 +0100

    creer compte updated => double post form enabled (for particulier et poste)

--- Sprint3/IHMWeb/html/creercompte.php
+++ Sprint3/IHMWeb/html/creercompte.php
@@ -98,7 +98,7 @@ if ($iphone || $android || $palmpre || $ipod || $berry == true)
   <div id="conteneur">    
 	<img class="ban" src="../styles/images/ban.png"></img>
 	
-	<?php	if (isset($_SESSION['LoggedIn']) && $_SESSION['LoggedIn'] == true) {
+	<?php	if (isset($_SESSION['LoggedIn']) && ($_SESSION['LoggedIn'] == true) && $_SESSION['category'] == 'PARTICULIER') {
 		
 			
 		?>
@@ -106,59 +106,118 @@ if ($iphone || $android || $palmpre || $ipod || $berry == true)
 		<div class="topnav" id="myTopnav">
 			<a href="../html/index.php"><img class="home" src="../styles/images/home.png"></img></a>
 			<a href="../html/contact.php">Contacts</a>
+			<a href="../html/mesBoites.php">Mes Boites</a>
+			<a href="../html/courrier.php">Courrier</a>
 			<a id="logs" href="../html/index.php"><?php echo $_SESSION['username']; ?></a>
 			<a id="logs" href="logout.php">Deconnexion</a>
 		</div>
-				<?php
-			}
-		 else
+		<?php
+		}
+				
+		else if (isset($_SESSION['LoggedIn']) && ($_SESSION['LoggedIn'] == true) && $_SESSION['category'] == 'POSTE')
 		 { ?>
+			<div class="topnav" id="myTopnav">
+			<a href="../html/index.php"><img class="home" src="../styles/images/home.png"></img></a>
+			<a href="../html/contact.php">Contacts</a>
+			<a href="../html/mesClients.php">Mes Clients</a>
+			<a id="logs" href="../html/index.php"><?php echo $_SESSION['username']; ?></a>
+			<a id="logs" href="logout.php">Deconnexion</a>
+		   </div>
+			<?php
+			
+		} else { ?>
 			<div class="topnav" id="myTopnav">
 			<a href="../html/index.php"><img class="home" src="../styles/images/home.png"></img></a>
 			<a href="../html/contact.php">Contacts</a>
 			<a id="logs" href="../html/creercompte.php">S'inscrire</a>
 			<a id="logs" href="../html/moncompte.php">Mon compte</a>
 		    </div>
-			<?php
-			
-		} ?>
+		<?php } ?>
 	<?php
 	$db = new Db();
-	if(!empty($_POST['pseudo']) && !empty($_POST['mdp']) && !empty($_POST['email']) &&!empty($_POST['nom']) &&!empty($_POST['prenom'])&&!empty($_POST['country'])&&!empty($_POST['numero']))
-	{
-		$firstname = $db -> quote($_POST['prenom']);
-		$lastname = $db -> quote($_POST['nom']);
-		$username = $db -> quote($_POST['pseudo']);
-		$password = $db -> quote($_POST['mdp']);
-		$country = $db -> quote($_POST['country']);
-		$tel = $db -> quote($_POST['numero']);
-		$email = $db -> quote($_POST['email']);
-		
-		$checkusername =  $db->select("SELECT * FROM users WHERE username = '".$username."'");
-		
-		if($checkusername != false)
-		{
-			echo "<h1>Error</h1>";
-			echo "<p>Sorry, that username is taken. Please go back and try again.</p>";
-		}
-		else
-		{
-			$hashed_password = password_hash($password, PASSWORD_DEFAULT);
-			$password = $db -> quote($hashed_password) ;
+if(isset($_POST['register_particulier']))
+{
+			if(!empty($_POST['pseudo']) && !empty($_POST['mdp']) && !empty($_POST['email']) &&!empty($_POST['nom']) &&!empty($_POST['prenom'])&&!empty($_POST['country'])&&!empty($_POST['tel']))
+    {
+			$firstname = $db -> quote($_POST['prenom']);
+			$lastname = $db -> quote($_POST['nom']);
+			$username = $db -> quote($_POST['pseudo']);
+			$password = $db -> quote($_POST['mdp']);
+			$country = $db -> quote($_POST['country']);
+			$tel = $db -> quote($_POST['tel']);
+			$email = $db -> quote($_POST['email']);
+			$category = $db -> quote("PARTICULIER");
+			$checkusername =  $db->select("SELECT * FROM users WHERE username = '".$username."'");
 			
-			$registerquery = $db->query("INSERT INTO `users` (`username`, `password`,`email`,`country`,`firstName`,`lastName`,`tel`) VALUES(".$username.", ".$password.",".$email.",".$country.",".$firstname.",".$lastname.",".$tel.")");
-			if($registerquery)
+			if($checkusername != false)
 			{
-				echo "<h1>Success</h1>";
-				echo "<p>Your account was successfully created. Please <a href=\"moncompte.php\">click here to login</a>.</p>";
+				echo "<h1>Error</h1>";
+				echo "<p>Sorry, that username is taken. Please go back and try again.</p>";
 			}
 			else
 			{
-				echo "<h1>Error</h1>";
-				echo "<p>Sorry, your registration failed. Please go back and try again.</p>";    
-			}       
-		}
+				$hashed_password = password_hash($password, PASSWORD_DEFAULT);
+				$password = $db -> quote($hashed_password) ;
+				
+				$registerquery = $db->query("INSERT INTO `users` (`username`, `password`,`email`,`country`,`firstName`,`lastName`,`tel`,`category`) VALUES(".$username.", ".$password.",".$email.",".$country.",".$firstname.",".$lastname.",".$tel.",".$category.")");
+				if($registerquery)
+				{
+					echo "<h1>Success</h1>";
+					echo "<p>Your account was successfully created. Please <a href=\"moncompte.php\">click here to login</a>.</p>";
+				}
+				else
+				{
+					echo "<h1>Error</h1>";
+					echo "<p>Sorry, your registration failed. Please go back and try again.</p>";    
+				}       
+			}
 	}
+}
+else if(isset($_POST['register_poste']))
+{
+	echo("from post0");
+	{
+		if(!empty($_POST['pseudo_poste']) && !empty($_POST['nom_poste']) && !empty($_POST['mdp_poste']) && !empty($_POST['email_poste']) &&!empty($_POST['activityRange_poste']) &&!empty($_POST['ville_poste'])&&!empty($_POST['country_poste'])&&!empty($_POST['numero_poste']))
+
+        {
+			$activity_range = $db -> quote($_POST['activityRange_poste']);
+			$nom_poste = $db -> quote($_POST['nom_poste']);
+			$username = $db -> quote($_POST['pseudo_poste']);
+			$password = $db -> quote($_POST['mdp_poste']);
+			$country = $db -> quote($_POST['country_poste']);
+			$tel = $db -> quote($_POST['tel_poste']);
+			$numero = $db -> quote($_POST['numero_poste']);
+			$ville = $db -> quote($_POST['ville_poste']);
+			$email = $db -> quote($_POST['email_poste']);
+			$category = $db -> quote("POSTE");
+			$checkusername =  $db->select("SELECT * FROM users WHERE username = '".$username."'");
+			echo("from post1");
+			if($checkusername != false)
+			{
+				echo "<h1>Error</h1>";
+				echo "<p>Sorry, that username is taken. Please go back and try again.</p>";
+			}
+			else
+			{
+				echo("from post2");
+				$hashed_password = password_hash($password, PASSWORD_DEFAULT);
+				$password = $db -> quote($hashed_password) ;
+				
+				$registerquery = $db->query("INSERT INTO `users` (`username`,`category`, `password`,`email`,`country`,`city`,`address_num`,`tel`,`activity_range`) VALUES(".$username.", ".$category.",".$password.",".$email.",".$country.",".$ville.",".$numero.",".$tel.",".$activity_range.")");
+				if($registerquery)
+				{
+					echo "<h1>Success</h1>";
+					echo "<p>Your account was successfully created. Please <a href=\"moncompte.php\">click here to login</a>.</p>";
+				}
+				else
+				{
+					echo "<h1>Error</h1>";
+					echo "<p>Sorry, your registration failed. Please go back and try again.</p>";    
+				}       
+			}
+        }
+    }
+}
 	else
 {
     ?>	
@@ -166,11 +225,13 @@ if ($iphone || $android || $palmpre || $ipod || $berry == true)
 		<div id="choice">
 			<p>Créer mon compte !</p>
 			<input type="submit" id="prticulier" onclick="particulier()" value="Particulier">
-			<input type="submit" id="poste" onclick="poste()" value="Poste">
+			<input type="submit" id="poste" onclick="poste()"            value="Poste       ">
 		</div>
 		<?php
-	}
+}
 		?>
+	
+	<p id="footer">Réalisé par l'équipe smartil</p>
   </div>
   </body>
 </html>
